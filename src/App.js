2bb814e6ab85be8717139eae9ac361659a95b927
codeguruvaa <mohanbvej@gmail.com>
2022-02-07 17:02:43 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+        <header>
+          <nav>
+           <center> Home | ABout | Services | Contact</center>
+          </nav>
+        </header>
+
+        <section className='slider'></section>
+
+        <div className='paras'>
+          <div className='pOne'>
+            <p>
+              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+            </p>
+          </div>
+          <div className='pTwo'>
+          <p>
+              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+            </p>
+          </div>
+        </div>
+
+        <div style={{clear:'both'}} ></div>
+
+        <section className='boxes'>
+          <div className='bOne'></div>
+          <div className='bTwo'></div>
+          <div className='bThree'></div>
+        </section>
+
+        <div style={{clear:'both'}} ></div>
+        
+        <footer></footer>
+
     </div>
   );
 }
 
 export default App;
+

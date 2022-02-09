e651d7aebe54fd6c67673c6f2a67a83781817991
codeguruvaa <mohanbvej@gmail.com>
2022-02-09 16:50:45 +0530

    init

--- src/App.js
+++ src/App.js
@@ -5,52 +5,18 @@ function App() {
   return (
     <div>
 
-        <header>
-          <nav>
-           <center> Home | ABout | Services | Contact</center>
-          </nav>
-        </header>
 
-        <section className='slider'></section>
+      <div className='header'></div>
 
-        <div className='paras'>
-          <div className='pOne'>
-            <p>
-              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-            </p>
-          </div>
-          <div className='pTwo'>
-          <p>
-              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-              the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
-            </p>
-          </div>
-        </div>
 
-        <div style={{clear:'both'}} ></div>
 
-        <section className='boxes'>
-          <div className='bOne'></div>
-          <div className='bTwo'></div>
-          <div className='bThree'></div>
-        </section>
-
-        <div style={{clear:'both'}} ></div>
-        
-        <footer></footer>
+      {/* <section>
+        <div className='bOne'></div>
+        <div className='bTwo'></div>
+      </section> */}
 
     </div>
   );
 }
 
 export default App;
-

56cc591b7d7225816a8335ce5a3ffd5b9927e2b4
codeguruvaa <mohanbvej@gmail.com>
2022-02-10 23:48:01 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
 import React from 'react';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
 
+     <div className='container'>
+      <h2 className='bg-danger text-white display-1'>Heading</h2>
+    </div>
 
-      <div className='header'></div>
+    <div className='container'>
+      <p className='bg-info p-3'>
+        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+      </p>
+    </div>
 
+    <div className='container'>
+      <div className='row'>
+        <p className='col'>
+        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+        </p>
+        <p className='col'>
+        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+        </p>
+        <p className='col'>
+        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+        </p>
+        <p className='col'>
+        the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+          the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog
+        </p>
+      </div>
+    </div>
 
-
-      {/* <section>
-        <div className='bOne'></div>
-        <div className='bTwo'></div>
-      </section> */}
 
     </div>
   );
